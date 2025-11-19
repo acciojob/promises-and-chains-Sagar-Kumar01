d67742605aef91b40fb3962c915b3b06bf9b2bd2
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-19 12:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+
+let age = document.getElementById("age");
+let name = document.getElementById("name");
+let btn = document.getElementById("btn");
+let forms = document.getElementById("forms");
+
+async function message(age,name) {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>=18){
+				resolve(name);
+			}else{
+				reject(name);
+			}
+		},4000)
+	})
+}
+
+
+btn.addEventListener("click", (e) => {
+	e.preventDefault();  
+
+	if (age.value === "" || name.value === "") {
+		alert("Please enter valid details.");
+	} else {
+		message(age.value, name.value)
+			.then(() => {
+				alert(`Welcome, ${name.value}. You can vote.`);
+				forms.submit();   
+			})
+			.catch(() => {
+				alert(`Oh sorry ${name.value}. You aren't old enough.`);
+				forms.submit();   
+			});
+	}
+});
+
